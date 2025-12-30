bd065d2d999edb9d0a99351f00743d591277e34d
Ayush Agrawal <ayushagr.work@gmail.com>
2025-12-31 01:51:10 +0530

    added evenlistener for multiple paras

--- L53/index.js
+++ L53/index.js
@@ -1,8 +1,38 @@
 
-function changeText(){
-  let fpara = document.getElementById('fpara');
-  fpara.textContent="Ayush";
-}
-fpara.addEventListener('click',changeText); // don't add paranthesis after the function name
+// function changeText(){
+//   let fpara = document.getElementById('fpara');
+//   fpara.textContent="Ayush";
+// }
+// fpara.addEventListener('click',changeText); // don't add paranthesis after the function name
 
-fpara.removeEventListener('click',changeText);
+// fpara.removeEventListener('click',changeText);
+//Best practice create function first then pass it in addEventListener and same function must be passed in removeEventListener as well.
+
+
+// fpara.addEventListener('click',function(){
+//   let fpara = document.getElementById('fpara');
+//   fpara.textContent="Ayush";
+// }
+// );                                                      // this is wrong as these methods are treated as two different objects.
+// fpara.removeEventListener('click',function(){
+//   let fpara = document.getElementById('fpara');
+//   fpara.textContent="Ayush";
+// });
+
+
+
+
+// let fpara = document.getElementById('fpara');
+let paras = document.querySelectorAll('p'); 
+function alertFunction(event){
+  alert("You have clicked on "+ event.target.textContent);
+}
+
+// for(let i=0;i<paras.length;i++){
+//   let para = paras[i];
+//   para.addEventListener('click',alertFunction);
+// }
+
+//Instead of iterating over all paras add event listener on div which contains all paras
+let mydiv = getElementById('wrapper')
+wrapper.addEventListener('click',alertFunction);
